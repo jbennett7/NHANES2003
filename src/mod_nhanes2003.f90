31c0971d25ab199d4762afb47497c6bee7fb90a1
Joseph J. Bennett <jbennett@jbennettconsulting.com>
2024-02-10 09:03:14 -0500

    refactor get_females

--- src/mod_nhanes2003.f90
+++ src/mod_nhanes2003.f90
@@ -10,36 +10,16 @@ module mod_nhanes2003
 contains
 
     subroutine get_females(filepath, fseqn)
-        character(len=*) :: filepath
+        character(len=*), intent(in) :: filepath
         integer, dimension(:), allocatable, intent(out) :: fseqn
         type(csv_file) :: f
-        character(len=30), dimension(:), allocatable :: header
-        integer, dimension(:), allocatable :: seqn, riagendr
         logical :: status_ok
-        integer, dimension(:), allocatable :: itypes
-        integer :: i, count
+        integer, dimension(:), allocatable :: seqn, riagendr
 
         call f%read(filepath, header_row=1, status_ok=status_ok)
-
-        call f%get_header(header, status_ok)
-        call f%variable_types(itypes, status_ok)
-
         call f%get(1, seqn, status_ok)
         call f%get(5, riagendr, status_ok)
-        count = 0
-        do i = 1, size(riagendr)
-            if (riagendr(i) == 2) then
-                count = count + 1
-            end if
-        end do
-        allocate(fseqn(count))
-        count = 0
-        do i = 1, size(seqn)
-            if (riagendr(i) == 2) then
-                count = count + 1
-                fseqn(count) = seqn(i)
-            end if
-        end do
+        fseqn = pack(seqn, riagendr == 2)
         call f%destroy()
     end subroutine get_females
 end module mod_nhanes2003
